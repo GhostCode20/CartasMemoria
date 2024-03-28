3d0e90f63ac2eea65207451866f5e43be66c4913
shary casianis <shary.casianispe@amigo.edu.co>
2024-03-28 13:52:48 -0500

    28marzparte2

--- tabla.py
+++
@@ -1,9 +0,0 @@
-from tabulate import tabulate
-
-# Crear una lista de listas con datos
-data = [['Juan', 25, 'Madrid'],
-        ['María', 30, 'Barcelona'],
-        ['Pedro', 35, 'Sevilla']]
-
-# Imprimir la tabla
-print(tabulate(data, headers=['Nombre', 'Edad', 'Ciudad']))
